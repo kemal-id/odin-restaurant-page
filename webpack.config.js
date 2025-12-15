bf7bd95c173c8c20b53fc6e29811147a29822134
kemal-id <54977402+kemal-id@users.noreply.github.com>
2025-12-15 15:07:43 +0700

    Initialize webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const htmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devTool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/main.html"],
+  },
+  plugins: [
+    new htmlPlugin({
+      template: "./src/main.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|jpg|jpeg)/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
